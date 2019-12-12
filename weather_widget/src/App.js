559d605ab2d01739c1ab99c18312482134343afc
Alice <alice.sabot@edu.ece.fr>
2019-12-12 22:27:16 +0100

    premier apercu

---
+++ weather_widget/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/header/Header'
+import NavBar from './components/navbar/NavBar'
+import Tableau from './components/Tableau'
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <NavBar/>
+      <Tableau/>
+    </div>
+  );
+}
+
+export default App;
---
+++ weather_widget/src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+class Header extends React.Component{
+
+    render(){
+
+        return(
+            <div>
+                <h1>Weather Widget</h1>
+            </div>
+        );
+}
+
+}
+
+export default Header;
